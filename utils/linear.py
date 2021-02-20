e69ea7b8b1f337ddf56682df3ef84795a4722e94
joelsewhere <joelsewhere@gmail.com>
2021-02-19 23:03:36 -0600

    Adding 1 dimenstional array to Linear

--- utils/linear.py
+++ utils/linear.py
@@ -5,27 +5,38 @@ class Linear:
         self.data = array
         self.T = self._transpose(array)
 
-    def _column_length(self, matrix):
+    def _column_length(self, array):
         '''Find the number of columns for a given array'''
 
         # Find the length of the first row
-        length = len(matrix[0])
+        if type(array[0]) == int:
+            return 0
+        length = len(array[0])
 
         # If any row in the matrix has 
         # a different length raise a ValueError
         # If all row lengths match, 
         # then return the length of the first row.
-        for row in matrix[1:]:
+        for row in array[1:]:
             if len(row) != length:
                 raise ValueError('Matrix contains mismatched column lengths.')
         return length
 
 
-    def _find_shape(self, matrix):
-        return (len(matrix), self._column_length(matrix))
+    def _find_shape(self, array):
+        row, col = len(array), self._column_length(array)
+        if  not col:
+            shape = (row, 0)
+      
+        else:
+            shape = (row, col)
+         
+        return shape
 
 
     def _transpose(self, array):
+        if not self.shape[1]:
+            return self.data
         return  [list(x) for x in list(zip(*array))]
 
     def dot(self, m2):
@@ -35,8 +46,22 @@ class Linear:
 # If the second matrice is compatible if transformed
 # then the matrice is transformed and the  operation is completed.
 # Otherwise, a ValueError is returned.
-
-        if self.shape[1] != self._find_shape(m2)[0]:
+        dot_product = []
+        if not self.shape[1]:
+            if self.shape[0] != len(m2):
+                assert ValueError(f"""array_1 shape {self.shape} and
+                                      array_2 shape {self._find_shape(m2)}
+                                      are incompatible""")
+            elif self._column_length(m2) in [0,1]:
+                count = 0
+                for i in range(self.shape[0]):
+                    if self._column_length(m2):
+                        count += self.data[i] * m2[i][0]
+                    else:
+                        count += self.data[i] * m2[i]
+                return count
+                
+        elif self.shape[1] != self._find_shape(m2)[0]:
             if self.shape[1] != self._find_shape(m2)[1]:
                 assert ValueError(f"""Matrix1 shape {self.shape} and 
                                     Matrix2 shape {self._find_shape(m2)} 
@@ -49,18 +74,19 @@ class Linear:
 # first matrix and the number of columns from the second matrix
 
         output_row = self.shape[0]
-        output_col = self._find_shape(m2)[1]
+        output_col = self._column_length(m2)
 # =============================================================================== #
 # Loop over each row of the first matrix and find the dot-product of 
 # a given row with each column of the second matrix. 
 
-        dot_product = []
-
         for row_idx in range(output_row):
             collected = []
             # Collect row
-            row = self.data[row_idx]
-
+            if not self.shape[1]:
+                row = self.data
+            else:
+                row = self.data[row_idx]
+            
             # Loop over columns of second matrix
             for col_idx in range(output_col):
 
@@ -68,9 +94,13 @@ class Linear:
                 column = [m2[x][col_idx] for x in range(len(m2))]
 
                 # Zip together corresponding idx values for m1 row and m2 column
+#                 if not self.shape[1]:
+#                     zipped = [(row, col) for col in  column]
+                    
+#                 else:
                 zipped = list(zip(row, column))
 
-                # Multiple each paired value 
+                # Multiply each paired value 
                 # and find the sum of the products
                 total = sum([x*y for x,y in zipped])
                 
@@ -79,6 +109,9 @@ class Linear:
 
             # Append complete rows
             dot_product.append(collected)
+            if not self.shape[1]:
+                dot_product = dot_product[0]
+                break
 
         return dot_product 
 
